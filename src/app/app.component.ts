41939cd9285965c567fbe0cf989b4cf09c92e478
abhishek-bits <abhi.shar1971@gmail.com>
2023-09-19 10:12:19 +0530

    add event listener for save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { DataState } from './enum/data-state.enum';
 import { Status } from './enum/status.enum';
+import { NgForm } from '@angular/forms';
+import { Server } from './interface/server';
 
 @Component({
   selector: 'app-root',
@@ -32,14 +34,15 @@ export class AppComponent implements OnInit {
   private filterSubject = new BehaviorSubject<string>("");
   // will be used to store the response we get from the server.
   private dataSubject = new BehaviorSubject<CustomResponse>(null);
-
   /*
    * But why did we take an observable and not a normal variable ???
-   * To be answered at the end of the course.
+   * Because we are using ngForms: RxJs
    */
   // filterStatus, which we are going to use in our UI,
   // is an observable of BehaviorSubject: filterSubject
   filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoading.asObservable();
 
   title = 'serverapp';
 
@@ -54,7 +57,12 @@ export class AppComponent implements OnInit {
         map(response => {
           // save the response inside dataSubject
           this.dataSubject.next(response);
-          return { dataState: DataState.LOADED_STATE, appData: response }
+          return { 
+            dataState: DataState.LOADED_STATE, 
+            // if we want the response to be in some sorted order.
+            // appData: {...response, data: { servers: response.data.servers.reverse() } } 
+            appData: response 
+          }
         }),
         // But while we are making the request to the backend,
         // we are going to return this observable.
@@ -79,7 +87,7 @@ export class AppComponent implements OnInit {
       .pipe(
         // What will we do with the response that we get ?
         // loop through all the servers that we have 
-        // and replace the server that we get from the backend
+        // and replace the server that we get from the backend (response)
         // inside of the array of servers (Server[]): this.dataSubject.value
         // so that we can update any updated information that we might get
         // from the backend.
@@ -119,6 +127,60 @@ export class AppComponent implements OnInit {
         }),
         startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
         catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
+  }
+
+  saveServer(serverForm: NgForm): void {
+
+    // Whoever in the UI is subscribed to this observable,
+    // then they are gonna get this true value.
+    // Then it's gonna show spinner to them.
+    this.isLoading.next(true);
+
+    // Can also do like this:
+    // this.appState$ = this.serverService.save$(<Server> serverForm.value)
+    // OR
+    // this.appState$ = this.serverService.save$(serverForm.value as Server)
+    // But, serverForm.value returns "any" type
+    // which will be internally converted into Server type.
+    this.appState$ = this.serverService.save$(serverForm.value)
+      .pipe(
+        // The response here will be the new server object
+        // added into the server.
+        map(response => {
+          // We'll add the new server into the current list of servers
+          
+          // this.dataSubject.value.data.servers.push(response.data.server);
+          
+          this.dataSubject.next(
+            {
+              ...response,
+              data: { servers: [...this.dataSubject.value.data.servers, response.data.server] }
+            }
+          );
+
+          // Close the Modal
+          document.getElementById('closeModal').click();
+
+          // Stop the spinner
+          this.isLoading.next(false);
+
+          // Reset the form
+          serverForm.resetForm({ status: this.Status.SERVER_DOWN })
+
+          // then pass the same value we just updated.
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ 
+          // Why? Because the data is already loaded.
+          dataState: DataState.LOADED_STATE, 
+          appData: this.dataSubject.value     // to get the CustomResponse object.
+        }),
+        catchError(( error: string ) => {
+          // Stop the spinner
+          this.isLoading.next(false);
           return of({ dataState: DataState.ERROR_STATE, error })
         })
       );
